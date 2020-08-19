a78ed046edc6e6aa0ecb450a06d7845e778c76b8
Ahmad Alawad <ahmad.alawad.sf@gmail.com>
2020-08-19 15:47:08 +0300

    Added so many comments

--- class-04/demo/pets-world/pets_world/pets.py
+++ class-04/demo/pets-world/pets_world/pets.py
@@ -25,31 +25,42 @@ from abc import abstractmethod, ABC
 
 
 
-class Pet(ABC):
+class Pet(ABC): # ABC is the big class that any class can inherit from
 
+    # Class Attribute
     count = 0
 
     def __init__(self, name):
         self.name = name
+        # Refer to count using the class name since it's a class attribute
         Pet.count += 1
 
+    # It's better to have __str__ for every sub class
     # def __str__(self):
     #     return f"Pet <{self.name}>"
 
     def __repr__(self):
+        # This method will be called if we call the class instance from the command line
         return f" '{self.name}' "
 
+    # speak is an abstract method because we want to force every sub class
+    # to have a declaration for it
     @abstractmethod
     def speak(self):
         pass
 
+    # A method without a decorator is by default an instance method
     def greet(self):
         return f"Hello, my name is {self.name}"
 
+    # get__characteristics is a static mthodbecause it doesn't interact with
+    # any attribute or methods in the class
     @staticmethod
     def get_characteristics():
         return "Likes to play with humans"
 
+    # get_count is a class method because it works on the class level
+    # we pass cls as a parameter to have access to attribues in class
     @classmethod
     def get_count(cls):
         return Pet.count
@@ -63,9 +74,12 @@ class Dog(Pet):
 
 class Cat(Pet):
     def __init__(self, name, nick_name):
-        self.nick_name = nick_name
-        # self.name = name
+        self.nick_name = nick_name # different that Dog class here
+        # self.name = name => let's put this in the super class
+        # super() refers to the super class
+        # __ini__ is called manually to inistantiate Cat instances using Pet class __init__
         super().__init__(name)
+
     def __str__(self):
         return f"Cat <{self.name}>"
 
--- class-04/demo/pets-world/tests/test_pets.py
+++ class-04/demo/pets-world/tests/test_pets.py
@@ -1,8 +1,11 @@
 from pets_world.pets import Pet, Dog, Cat
 import pytest
 
+# you can skip test cases using this decorator
 # @pytest.mark.skip(reason='Testing Count')
 def test_cat_greet(prep_data):
+    # prep_data fixture is passed to provide this test case with needed data
+    # prep_data return a dictionary of all instances
     expected = 'Meooow, my nick name is Shosho'
     actual = prep_data['sherry'].speak()
     assert actual == expected
@@ -13,6 +16,7 @@ def test_greet(prep_data):
     actual = prep_data['oscar'].greet()
     assert actual == expected
 
+# A fixture is a way to provide test cases with some initial data
 @pytest.fixture
 def prep_data():
     sherry = Cat('Sherry R', 'Shosho') # preparing data
@@ -23,5 +27,5 @@ def prep_data():
     return {'oscar':oscar, 'sherry':sherry, 'd1':d1, 'c':c, 'd2':d2}
 
 def test_count(prep_data):
-    assert Pet.count == 15
+    assert Pet.count == 15 # because we called prep_data 3 times
 
