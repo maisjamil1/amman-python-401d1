75d0decd01c1bdd7f03ab29c295d58e7151a8291
Ahmad Alawad <ahmad.alawad.sf@gmail.com>
2020-08-31 13:03:49 +0300

    Added demo for class 12, pandas

---
+++ class-12/demo/shelter.py
@@ -0,0 +1,31 @@
+# class Dog:
+#     pass
+
+# class Cat:
+#     pass
+
+
+# class Shelter:
+
+
+#     enqueue(animal)
+#         add this animal to the queue
+
+
+#     front -> 'cat12456' -> 'cat7654' -> 'dog988787' -> 'caty234' -> 'dog1' -> 'dog2' <- rear
+
+#     dequeue(pref)
+
+
+# shelter.dequeue('dog')
+# 'dog988787'
+
+# front -> 'cat12456' -> 'cat7654' -> 'caty234' -> 'dog1' -> 'dog2' <- rear
+
+# shelter.dequeue('cat')
+# 'cat12456'
+
+# front -> 'cat7654' -> 'caty234' -> 'dog1' -> 'dog2' <- rear
+
+# shelter.dequeue()
+# None
